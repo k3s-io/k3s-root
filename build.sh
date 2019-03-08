7203bff8bbcda5a8938b26a375f0e084d593bf9d
Darren Shepherd <darren@rancher.com>
2019-03-08 15:33:13 -0700

    rootless support
    Adds slirp4netns and enables nsenter for busybox

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x -e
 
+mkdir -p dist
 for ARCH in amd64 arm arm64; do
     docker build --build-arg ARCH=${ARCH} -t k3s-root .
-    docker run -i --rm k3s-root tar cf - -C /usr/src ./bin > dist/k3s-root-${ARCH}.tar
+    docker run -i --rm -v k3s-root-cache:/usr/src/ccache k3s-root tar cf - -C /usr/src ./bin > dist/k3s-root-${ARCH}.tar
 done
