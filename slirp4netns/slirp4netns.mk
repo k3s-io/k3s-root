bac8b033f0c6e93b9f8f2fbf222e18aae82b8838
Darren Shepherd <darren@rancher.com>
2020-01-22 10:50:10 -0700

    Bump to slirp4netns v0.4.3

--- slirp4netns/slirp4netns.mk
+++ slirp4netns/slirp4netns.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SLIRP4NETNS_VERSION = 6a2bb52fa6d888dc355344c2c45c8a80d1b41c3a
+SLIRP4NETNS_VERSION = v0.4.3
 SLIRP4NETNS_SITE = git://github.com/rootless-containers/slirp4netns.git
 SLIRP4NETNS_LICENSE = BSD-4-Clause, BSD-2-Clause
 
