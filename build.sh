6105eefc24d74a47ad79d60099abb866affd74c8
Erik Wilson <Erik.E.Wilson@gmail.com>
2020-05-12 14:46:31 -0700

    Cleanup build system

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -x -e
 
 mkdir -p dist
 for ARCH in amd64 arm arm64; do
-    docker build --build-arg ARCH=${ARCH} -t k3s-root .
-    docker run -i --rm -v k3s-root-cache:/usr/src/ccache k3s-root tar cf - -C /usr/src ./bin ./etc > dist/k3s-root-${ARCH}.tar
+    docker build --build-arg ARCH=${ARCH} --tag k3s-root .
+    docker run --rm k3s-root \
+        tar cf - -C /usr/src ./bin ./etc > dist/k3s-root-${ARCH}.tar
 done
