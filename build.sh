d48ba22790b13b65c2df34517185d1cd2e7265fb
Erik Wilson <Erik.E.Wilson@gmail.com>
2020-05-12 16:31:59 -0700

    Add ppc64le arch to build process

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x -e
 
+BUILD_ARCHS=${BUILD_ARCHS:-amd64 arm64 arm ppc64le}
+
 mkdir -p dist
-for ARCH in amd64 arm arm64; do
-    docker build --build-arg ARCH=${ARCH} --tag k3s-root .
+for arch in ${BUILD_ARCHS}; do
+    docker build --build-arg ARCH=${arch} --tag k3s-root .
     docker run --rm k3s-root \
-        tar cf - -C /usr/src ./bin ./etc > dist/k3s-root-${ARCH}.tar
+        tar cf - -C /usr/src ./bin ./etc > dist/k3s-root-${arch}.tar
 done
